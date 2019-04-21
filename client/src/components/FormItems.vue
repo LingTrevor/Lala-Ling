158d744c55c86f13a82358ecce76facdde5a94da
Trevor <lingtrevor@gmail.com>
2019-04-21 20:10:18 +0300

    Added FormItems component for AddTodo and EditTodo

---
+++ client/src/components/FormItems.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="form-items">
+    <slot>No Content Found</slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.form-items {
+  padding: 10px;
+  box-sizing: border-box;
+  width: 100%;
+  display: block;
+  margin-bottom: 10px;
+}
+</style>
--- client/src/views/AddTodo.vue
+++ client/src/views/AddTodo.vue
@@ -1,28 +1,30 @@
 <template>
   <TodoForm formTitle="Add Todo">
-    <div class="form-items">
+    <FormItems>
       <label for>Todo title</label>
       <input type="text" />
-    </div>
-    <div class="form-items">
+    </FormItems>
+    <FormItems>
       <label for>Additional info</label>
       <input type="text" />
-    </div>
-    <div class="form-items">
+    </FormItems>
+    <FormItems>
       <label for>Due at</label>
       <input type="date" />
-    </div>
+    </FormItems>
     <FormBtn btnTitle="Save" />
   </TodoForm>
 </template>
 
 <script>
 import TodoForm from "@/components/layouts/TodoForm";
+import FormItems from "@/components/FormItems";
 import FormBtn from "@/components/FormBtn";
 export default {
   name: "AddForm",
   components: {
     TodoForm,
+    FormItems,
     FormBtn
   }
 };
@@ -30,28 +32,20 @@ export default {
 
 <style lang="scss" scoped>
 @import "../scss/_variables.scss";
-.form-items {
-  padding: 10px;
-  box-sizing: border-box;
-  width: 100%;
+label {
   display: block;
-  margin-bottom: 10px;
+  margin: 5px 0;
+  line-height: 20px;
+  color: $color-primary-text;
+}
 
-  label {
-    display: block;
-    margin: 5px 0;
-    line-height: 20px;
-    color: $color-primary-text;
-  }
-
-  input {
-    display: block;
-    width: 100%;
-    line-height: 20px;
-    font-size: 18px;
-    padding: 5px;
-    color: $color-primary-text;
-    box-sizing: border-box;
-  }
+input {
+  display: block;
+  width: 100%;
+  line-height: 20px;
+  font-size: 18px;
+  padding: 5px;
+  color: $color-primary-text;
+  box-sizing: border-box;
 }
 </style>
