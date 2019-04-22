979ff4d0f4c5b5de5da0e87a236bfacf937faea6
Trevor <lingtrevor@gmail.com>
2019-04-22 19:42:09 +0300

    Npm init and app.js setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {port} = require('./server/config');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`Started on port: ${port}`);
+});
