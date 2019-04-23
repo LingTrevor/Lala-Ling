576fa59fe532f7336ec9a92fb9e7d9817c345359
Trevor <lingtrevor@gmail.com>
2019-04-23 09:19:35 +0300

    NotFound view

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <Container>
+    <Row>
+      <h1>Error 404 - Not Found</h1>
+    </Row>
+  </Container>
+</template>
+
+<script>
+import Container from "@/components/layouts/Container";
+import Row from "@/components/layouts/Row";
+export default {
+  name: "NotFound",
+  components: {
+    Container,
+    Row
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+h1 {
+  text-align: center;
+  color: $color-primary-text;
+  padding: 5px;
+  margin-bottom: 20px;
+}
+</style>
